6c3d65014babbca9ee04f3a7a0369042dc81d46d
Kaewhyy <101496442+Kaewhyy@users.noreply.github.com>
2025-02-27 21:45:46 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 131004
+let accountEmail = "happierlike@gmail.com"
+var accountPassword ="280607"
+accountCity = "Kolkata"
+let accountState;
+
+// accountId = 13
+
+accountEmail ="kapoor@yah00.com"
+accountPassword ="287134"
+accountCity = "siliguri"
+
+/*
+not to use var because of issues in block scope and functional scope
+*/
+
+console.log(accountId);
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState ])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) // we are nodejs, not browser
+
+console.log(3 + 3)//code redability matters
+
+console.log("KY");
+
+let name = "Kshitiz"
+let age = 20
+let isLoggedIn = true
+
+// number=> 2 to the power 53 {range}
+// bigint for large calcs / numbers like stocks and platforms like reddit
+// strings => ""(to use)
+// bolean = true/false
+// null = standalone value 
+// undefined = value not assigned
+// symbol = unique datatype
+
+//object
+
+console.log(typeof "kshitiz");
+console.log(typeof undefined);//undefined (undefined already is a datatype)
+console.log(typeof null);//null is an object
+
+
